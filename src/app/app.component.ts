879442db5652bc4b2b1073c1aa5dd5da401e1f36
s2ftech <s2ftech@gmail.com>
2024-04-18 22:06:20 +0530

    s

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+// app.component.ts
 import { Component, OnInit } from '@angular/core';
 import { Quoteservice } from './services/auth.service';
 import { FormBuilder, FormGroup ,Validators } from '@angular/forms';
@@ -9,103 +10,92 @@ import { quote } from './models/quote';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  finalArray:quote[]=[];
-  filteredArray: any[] | undefined; 
-    searchQuery: string = '';
+  finalArray: quote[] = [];
+  filteredArray: any[] | undefined;
+  searchQuery: string = '';
+  quoteFormGroup: FormGroup;
 
-  quoteFormGroup :FormGroup;
-  constructor(private quoservice : Quoteservice, private fb: FormBuilder){
+  constructor(private quoservice: Quoteservice, private fb: FormBuilder) {
     this.quoteFormGroup = this.fb.group({
-      Id : [""],
-      author : ["",Validators.required],
-      tags : ["",Validators.required],
-      quoteText : ["",Validators.required],
-    })
-
-
+      Id: [""],
+      author: ["", Validators.required],
+      tags: ["", Validators.required],
+      quoteText: ["", Validators.required],
+    });
   }
-  
+
   ngOnInit(): void {
+    this.searchQuery="";
     this.GetQuotes();
     this.filteredArray = this.finalArray;
-    
-    
-
   }
-  GetQuotes()
-  {
-    this.quoservice.GetQuote().subscribe(response =>{
+
+  GetQuotes() {
+    this.quoservice.GetQuote().subscribe(response => {
       console.log(response);
       this.finalArray = response;
-    })
+    });
   }
-  onSubmit() {
-  debugger;
-  const formData = this.quoteFormGroup.value;
-  if (formData && Object.keys(formData).length !== 0) {
-    if (formData.Id) {
-      this.quoservice.UpdateQuote(formData).subscribe(response => {
-        console.log(response);
-        this.GetQuotes();
-        this.resetForm();
-      });
-    } else {
-      this.quoservice.CreateQuote(formData).subscribe(response => {
-        console.log(response);
-        this.GetQuotes();
-        this.resetForm();
-      });
-    }
-  } else {
-    console.error("Form data is empty.");
-  }
-}
 
-resetForm() {
-  this.quoteFormGroup.reset({
-    Id: "",
-    author: "",
-    tags: "",
-    quoteText: ""
-  });
-}
-filterData(): void {
-  if (this.searchQuery.trim() !== '') {
+  onSubmit() {
+    if (this.quoteFormGroup.valid) {
+      const formData = this.quoteFormGroup.value;
+      if (formData.Id) {
+        this.quoservice.UpdateQuote(formData).subscribe(response => {
+          console.log(response);
+          this.searchQuery=""
+          this.GetQuotes();
+          this.resetForm();
+        });
+      } else {
+        this.quoservice.CreateQuote(formData).subscribe(response => {
+          console.log(response);
+          this.searchQuery="";
+          this.GetQuotes();
+          this.resetForm();
+        });
+      }
+    } else {
+      console.error("Form data is invalid.");
+    }
+  }
+
+  resetForm() {
+    this.quoteFormGroup.reset({
+      Id: "",
+      author: "",
+      tags: "",
+      quoteText: ""
+    });
+  }
+
+  filterData(): void {
+    if (this.searchQuery.trim() !== '') {
       this.filteredArray = this.finalArray.filter(data =>
-          data.author.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-          data.tags.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
-          data.quoteText.toLowerCase().includes(this.searchQuery.toLowerCase())
+        data.author.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        data.tags.toLowerCase().includes(this.searchQuery.toLowerCase()) ||
+        data.quoteText.toLowerCase().includes(this.searchQuery.toLowerCase())
       );
-  } else {
+    } else {
       // If search query is empty, show all data
       this.filteredArray = this.finalArray;
+    }
   }
-}
 
-  FillForm(data:any){
+  FillForm(data: any) {
     this.quoteFormGroup.setValue({
-      Id : data.id,
-      author : data.author,
-      tags : data.tags,
-      quoteText : data.quoteText,
-    })
+      Id: data.id,
+      author: data.author,
+      tags: data.tags,
+      quoteText: data.quoteText,
+    });
   }
 
-  DeleteQuote(data:any){
-    debugger
+  DeleteQuote(data: any) {
     let Id = data.id;
-    this.quoservice.DeleteQuote(Id).subscribe(res =>{
+    this.quoservice.DeleteQuote(Id).subscribe(res => {
       console.log(res);
       this.GetQuotes();
-    }
-
-    )
-
+    });
   }
-
-
-
-
-
-  title = 'frontend';
 }
